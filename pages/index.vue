7d6b219ee51fd961e2896dac7df033e1bba4f99a
Lykkechen <lykkechen@MBP-di-Le.lan>
2021-07-06 14:16:31 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  head () {
+    return {
+      title: 'Home |'
+    }
+  }
+})
+export default class IndexPage extends Vue {}
+</script>

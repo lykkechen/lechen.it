098f6b2208a27bdce07ef8914c10a2bfe7373cf7
Lykkechen <lykkechen@MBP-di-Le.lan>
2021-07-07 14:20:31 +0200

    fix: remove nuxt content module

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,13 +33,7 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
   ],
-
-  // Content module configuration (https://go.nuxtjs.dev/config-content)
-  content: {},
-
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
